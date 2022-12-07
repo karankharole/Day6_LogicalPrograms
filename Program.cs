c6a102b338768e8664b9e8ae61954bd1dc852826
karankharole <karankharole2000@gmail.com>
2022-12-07 19:19:41 +0530

    generate a coupon number usinf random

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Day6Problems num = new Day6Problems();
-            num.Simulate_Stopwatch();
+            num.generateNDistinctCouponNum()
 
         }
     }
